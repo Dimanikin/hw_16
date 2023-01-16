d12b638398d3c04c43539329fbab203a2fa26794
Dimanikin <driver-s@mail.ru>
2023-01-16 11:26:38 +0100

    Fix config.py file

--- config.py
+++ config.py
@@ -1,13 +1,16 @@
+import os.path
+
 from flask import Flask
 from setup_db import db
 from utils import GetData
 
-app = Flask(__name__)
+app = Flask(__name__,instance_path=os.path.dirname(os.path.abspath(__file__)))
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['JSON_AS_ASCII']
 db.init_app(app)
 
 data = GetData()
 
-# with app.app_context():
-#     data.create_db()
+with app.app_context():
+    data.create_db()
